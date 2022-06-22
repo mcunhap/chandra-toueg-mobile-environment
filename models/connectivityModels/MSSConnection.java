5dfaf405036cdaf5b0555870347916c0ec33a0f1
Matheus Cunha <matheuscunha96@hotmail.com>
2022-06-21 21:29:36 -0300

    add MHNode and update MSSConnection to connect to MHNodes that is inside the radius

--- models/connectivityModels/MSSConnection.java
+++ models/connectivityModels/MSSConnection.java
@@ -1,22 +1,54 @@
 package projects.chandra_toueg.models.connectivityModels;
 
+import projects.chandra_toueg.nodes.nodeImplementations.MHNode;
+import projects.chandra_toueg.nodes.nodeImplementations.MSSNode;
+import sinalgo.configuration.Configuration;
 import sinalgo.exception.CorruptConfigurationEntryException;
 import sinalgo.models.ConnectivityModelHelper;
 import sinalgo.nodes.Node;
+import sinalgo.runtime.Global;
+import sinalgo.runtime.Main;
 import sinalgo.runtime.SinalgoRuntime;
 
 import java.util.HashMap;
 
 public class MSSConnection extends ConnectivityModelHelper {
     private static boolean initialized;
+    private static double rMaxSquare; // we reuse the rMax value from the GeometricNodeCollection.
 
     public MSSConnection() throws CorruptConfigurationEntryException {
         if(!initialized) {
+            double geomNodeRMax = Configuration.getDoubleParameter("GeometricNodeCollection/rMax");
+
+            try {
+                rMaxSquare = Configuration.getDoubleParameter("UDG/rMax");
+            } catch (CorruptConfigurationEntryException e) {
+                Global.getLog().logln(
+                        "\nWARNING: Did not find an entry for UDG/rMax in the XML configuration file. Using GeometricNodeCollection/rMax.\n");
+                rMaxSquare = geomNodeRMax;
+            }
+
+            if (rMaxSquare > geomNodeRMax) { // dangerous! This is probably not what the user wants!
+                Main.minorError(
+                        "WARNING: The maximum transmission range used for the UDG connectivity model is larger than the maximum transmission range specified for the GeometricNodeCollection.\nAs a result, not all connections will be found! Either fix the problem in the project-specific configuration file or the '-overwrite' command line argument.");
+            }
+
+            rMaxSquare = rMaxSquare * rMaxSquare;
             initialized = true;
         }
     }
 
     // all MSS Nodes are connected to each other
     @Override
-    protected boolean isConnected(Node from, Node to) { return true; }
+    protected boolean isConnected(Node from, Node to) {
+        if (from instanceof MSSNode && to instanceof MSSNode) {
+            return true;
+        } else if (from instanceof MSSNode && to instanceof MHNode || from instanceof MHNode && to instanceof MSSNode) {
+            double dist = from.getPosition().squareDistanceTo(to.getPosition());
+            System.out.println("From: " + from.getID() + " To: " + to.getID() + " Dist: " + dist + " RMaxSquare: " + rMaxSquare + " Connected: " + (dist < rMaxSquare));
+            return dist < rMaxSquare;
+        }
+
+        return false;
+    }
 }
---
+++ nodes/nodeImplementations/MHNode.java
@@ -0,0 +1,119 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2007-2013, Distributed Computing Group (DCG)
+                         ETH Zurich
+                         Switzerland
+                         dcg.ethz.ch
+              2017-2018, André Brait
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package projects.chandra_toueg.nodes.nodeImplementations;
+
+import lombok.Getter;
+import lombok.Setter;
+import projects.chandra_toueg.nodes.messages.*;
+import sinalgo.exception.WrongConfigurationException;
+import sinalgo.gui.transformation.PositionTransformation;
+import sinalgo.nodes.Node;
+import sinalgo.nodes.edges.Edge;
+import sinalgo.nodes.messages.Inbox;
+import sinalgo.nodes.messages.Message;
+import sinalgo.tools.storage.ReusableListIterator;
+
+import java.awt.*;
+
+@Getter
+@Setter
+public class MHNode extends Node {
+    boolean propose = false;
+
+    @Override
+    public void handleMessages(Inbox inbox) {
+        while (inbox.hasNext()) {
+            Message msg = inbox.next();
+            Node sender = inbox.getSender();
+
+            if (msg instanceof ProposeValueMessage) {
+            }
+        }
+    }
+
+    @Override
+    public void preStep() {
+        if (!propose && connectedToMSS()) {
+        }
+    }
+    
+    private boolean connectedToMSS() {
+        ReusableListIterator<Edge> connections = this.getOutgoingConnections().iterator();
+        
+        while (connections.hasNext()) {
+            Edge connection = connections.next();
+
+            if (connection.getEndNode() instanceof MSSNode) {
+                System.out.println("Node: " + this.getID() + " Connected to MSS: " + connection.getEndNode().getID());
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void neighborhoodChange() {
+    }
+
+    public void start() {
+    }
+
+    @Override
+    public void draw(Graphics g, PositionTransformation pt, boolean highlight) {
+        super.drawNodeAsDiskWithText(g, pt, highlight, "MH: " + this.getID(), 25, Color.GREEN);
+    }
+
+    @Override
+    public void postStep() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public void checkRequirements() throws WrongConfigurationException {
+    }
+}
